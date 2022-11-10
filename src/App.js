4bc287a188723e8acaf63fec85c37c5e3f2845bd
santomi3112 <adiprasetio007@gmail.com>
2022-11-10 12:53:53 +0700

    added key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import "./App.css";
 import { useState } from "react";
 import { List } from "./components/List";
-import uniqid from "uniqid"
+import uniqid from "uniqid";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState({text:'',id: uniqid()});
+  const [newTask, setNewTask] = useState({ text: "", id: uniqid() });
 
   const handleChange = (e) => {
-    setNewTask({text: e.target.value,id: newTask.id});
+    setNewTask({ text: e.target.value, id: newTask.id });
   };
 
   const addTask = (e) => {
     e.preventDefault();
     setTasks(tasks.concat(newTask));
-    setNewTask({text: '', id: uniqid()});
+    setNewTask({ text: "", id: uniqid() });
     console.log([...tasks, newTask]);
   };
 
-  const deleteTask = (taskName) => {
-    setTasks(tasks.filter((tasks) => tasks !== taskName));
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((tasks) => tasks !== id));
   };
 
   return (
@@ -29,14 +29,19 @@ function App() {
 
       <div className="addTask">
         <form onSubmit={addTask}>
-          <input type="text" onChange={handleChange} value={newTask.text} id="taskInput" />
+          <input
+            type="text"
+            onChange={handleChange}
+            value={newTask.text}
+            id="taskInput"
+          />
           <button onClick={addTask}>Add task</button>
         </form>
       </div>
       <div className="list">
         <ul>
           {tasks.map((data) => {
-            return <List data={data.text} delete={deleteTask} />;
+            return <List key={data.id} data={data.text} delete={deleteTask} />;
           })}
         </ul>
       </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,7 @@
 export const List = props => {
     return (
         <div>
-            <li>{props.data}</li>
+            <li key={props.id} >{props.data}</li>
             <button onClick={() => props.delete(props.data)}>delete {props.data}</button>
         </div>
     );
